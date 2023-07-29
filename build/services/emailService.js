b8351d602e02e9d594a5a53bf16674b8dab520bf
Aniruddha S J <aniruddhajaganatha@gmail.com>
2023-07-29 16:03:19 +0530

    shifting from aws ses to nodemailer

--- build/services/emailService.js
+++ build/services/emailService.js
@@ -1,4 +1,48 @@
 "use strict";
+// import { SESClient, SendEmailCommand } from "@aws-sdk/client-ses";
+// import { error } from "console";
+// // require("dotenv").config();
+// // console.log(process.env.AWS_ACCESS_KEY_ID);
+// const ses = new SESClient({ region: "eu-north-1" });
+// function SendEmailCommandFunc(
+//   souceEmail: string,
+//   destEmail: string,
+//   message: string
+// ) {
+//   return new SendEmailCommand({
+//     Source: souceEmail,
+//     Destination: {
+//       ToAddresses: [destEmail],
+//     },
+//     Message: {
+//       Subject: {
+//         Charset: "UTF-8",
+//         Data: "Twitter Clone Login OTP",
+//       },
+//       Body: {
+//         Text: {
+//           Charset: "UTF-8",
+//           Data: message,
+//         },
+//       },
+//     },
+//   });
+// }
+// export async function sendEmailToken(email: string, token: string) {
+//   const message = `Your One Time Password is ${token}`;
+//   const command = await SendEmailCommandFunc(
+//     "aniruddhajaganatha@gmail.com",
+//     email,
+//     message
+//   );
+//   try {
+//     return await ses.send(command);
+//   } catch (e) {
+//     console.log("error while sending the email: " + e);
+//     return error;
+//   }
+// }
+// // sendEmailToken("1rn20cs021.aniruddha@gmail.com", "89293829");
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
     function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
@@ -10,37 +54,26 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.sendEmailToken = void 0;
-const client_ses_1 = require("@aws-sdk/client-ses");
 const console_1 = require("console");
-// require("dotenv").config();
-// console.log(process.env.AWS_ACCESS_KEY_ID);
-const ses = new client_ses_1.SESClient({ region: "eu-north-1" });
-function SendEmailCommandFunc(souceEmail, destEmail, message) {
-    return new client_ses_1.SendEmailCommand({
-        Source: souceEmail,
-        Destination: {
-            ToAddresses: [destEmail],
-        },
-        Message: {
-            Subject: {
-                Charset: "UTF-8",
-                Data: "Twitter Clone Login OTP",
-            },
-            Body: {
-                Text: {
-                    Charset: "UTF-8",
-                    Data: message,
-                },
-            },
-        },
-    });
-}
+const nodemailer = require("nodemailer");
+const transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+        user: "aniruddhajaganatha@gmail.com",
+        pass: "dcpnlwbfoqzqkijm",
+    },
+});
 function sendEmailToken(email, token) {
     return __awaiter(this, void 0, void 0, function* () {
         const message = `Your One Time Password is ${token}`;
-        const command = yield SendEmailCommandFunc("aniruddhajaganatha@gmail.com", email, message);
+        const options = {
+            from: "aniruddhajaganatha@gmail.com",
+            to: email,
+            subject: "Twitter Clone Login OTP",
+            text: message,
+        };
         try {
-            return yield ses.send(command);
+            return transporter.sendMail(options);
         }
         catch (e) {
             console.log("error while sending the email: " + e);
@@ -49,4 +82,3 @@ function sendEmailToken(email, token) {
     });
 }
 exports.sendEmailToken = sendEmailToken;
-// sendEmailToken("1rn20cs021.aniruddha@gmail.com", "89293829");
--- src/services/emailService.ts
+++ src/services/emailService.ts
@@ -1,44 +1,69 @@
-import { SESClient, SendEmailCommand } from "@aws-sdk/client-ses";
+// import { SESClient, SendEmailCommand } from "@aws-sdk/client-ses";
+// import { error } from "console";
+// // require("dotenv").config();
+// // console.log(process.env.AWS_ACCESS_KEY_ID);
+// const ses = new SESClient({ region: "eu-north-1" });
+// function SendEmailCommandFunc(
+//   souceEmail: string,
+//   destEmail: string,
+//   message: string
+// ) {
+//   return new SendEmailCommand({
+//     Source: souceEmail,
+//     Destination: {
+//       ToAddresses: [destEmail],
+//     },
+//     Message: {
+//       Subject: {
+//         Charset: "UTF-8",
+//         Data: "Twitter Clone Login OTP",
+//       },
+//       Body: {
+//         Text: {
+//           Charset: "UTF-8",
+//           Data: message,
+//         },
+//       },
+//     },
+//   });
+// }
+// export async function sendEmailToken(email: string, token: string) {
+//   const message = `Your One Time Password is ${token}`;
+//   const command = await SendEmailCommandFunc(
+//     "aniruddhajaganatha@gmail.com",
+//     email,
+//     message
+//   );
+//   try {
+//     return await ses.send(command);
+//   } catch (e) {
+//     console.log("error while sending the email: " + e);
+//     return error;
+//   }
+// }
+// // sendEmailToken("1rn20cs021.aniruddha@gmail.com", "89293829");
+
 import { error } from "console";
-// require("dotenv").config();
-// console.log(process.env.AWS_ACCESS_KEY_ID);
-const ses = new SESClient({ region: "eu-north-1" });
-function SendEmailCommandFunc(
-  souceEmail: string,
-  destEmail: string,
-  message: string
-) {
-  return new SendEmailCommand({
-    Source: souceEmail,
-    Destination: {
-      ToAddresses: [destEmail],
-    },
-    Message: {
-      Subject: {
-        Charset: "UTF-8",
-        Data: "Twitter Clone Login OTP",
-      },
-      Body: {
-        Text: {
-          Charset: "UTF-8",
-          Data: message,
-        },
-      },
-    },
-  });
-}
+const nodemailer = require("nodemailer");
+const transporter = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: "aniruddhajaganatha@gmail.com",
+    pass: "dcpnlwbfoqzqkijm",
+  },
+});
 export async function sendEmailToken(email: string, token: string) {
   const message = `Your One Time Password is ${token}`;
-  const command = await SendEmailCommandFunc(
-    "aniruddhajaganatha@gmail.com",
-    email,
-    message
-  );
+  const options = {
+    from: "aniruddhajaganatha@gmail.com",
+    to: email,
+    subject: "Twitter Clone Login OTP",
+    text: message,
+  };
   try {
-    return await ses.send(command);
+    return transporter.sendMail(options);
   } catch (e) {
     console.log("error while sending the email: " + e);
     return error;
   }
 }
-// sendEmailToken("1rn20cs021.aniruddha@gmail.com", "89293829");
